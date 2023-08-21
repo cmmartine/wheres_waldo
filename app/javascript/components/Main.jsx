6858db2f0f6a72f351ba1ecda9866d2941e1b851
Chris Martine <cmmartine@gmail.com>
2023-08-21 14:11:30 -0400

    Add user selection box creation

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import waldoConvention from '../../assets/images/Waldo_Convention.jpg';
+import { createBox, removeBox } from "./userSelectionBox";
 
 export default function Main() {
-    return(
-        <div className='waldo-img-container'>
-          <img src={waldoConvention} />
-        </div>
-    )
+
+  function handleUserClick(e) {
+    const x = e.pageX;
+    const y = e.pageY;
+    removeBox();
+    createBox({x: x, y: y});
+  }
+
+  document.addEventListener('click', (e) => {handleUserClick(e)});
+
+  return(
+    <div className='waldo-img-container'>
+      <img src={waldoConvention} />
+    </div>
+  )
 };
---
+++ app/javascript/components/userSelectionBox.js
@@ -0,0 +1,42 @@
+const halfBoxSize = {x: 55, y: 85};
+
+export function createBox(userSelectionCoords) {
+  const boxCoordinates = calcBoxCoordinates(userSelectionCoords);
+  drawBox(boxCoordinates);
+}
+
+export function removeBox() {
+  const box = document.getElementById('user-selection-box');
+  if(box) {
+    box.remove();
+  } else {
+    return
+  }
+}
+
+function calcBoxCoordinates(userSelectionCoords) {
+  const leftX = userSelectionCoords.x - halfBoxSize.x;
+  const rightX = userSelectionCoords.x + halfBoxSize.x;
+  const topY = userSelectionCoords.y - halfBoxSize.y;
+  const bottomY = userSelectionCoords.y + halfBoxSize.y;
+  const boxCoordinates = {left: leftX, right: rightX, top: topY, bottom: bottomY};
+
+  return boxCoordinates;
+}
+
+function drawBox(boxCoords) {
+  console.log(boxCoords)
+  const root = document.getElementById('root');
+  const box = document.createElement('div');
+  box.id = 'user-selection-box';
+  box.style.position = 'absolute';
+  box.style.left = boxCoords.left + 'px';
+  box.style.right = boxCoords.right + 'px';
+  box.style.top = boxCoords.top + 'px';
+  box.style.bottom = boxCoords.bottom + 'px';
+  box.style.width = '110px';
+  box.style.height = '166px';
+  box.style.border = '5px solid greenyellow';
+  root.appendChild(box);
+}
+
